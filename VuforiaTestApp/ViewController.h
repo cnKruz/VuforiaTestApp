b7a2c450edec32f715d1e9bb0019fbde9fb47865
Eduardo <eduardo@administradors-Mac-mini.local>
2015-02-03 10:26:44 -0600

    Initial Commit

---
+++ VuforiaTestApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VuforiaTestApp
+//
+//  Created by Eduardo on 2/3/15.
+//  Copyright (c) 2015 Eduardo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
